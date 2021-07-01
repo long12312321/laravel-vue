812a0fdf48101fd68482e2d051c02fc82414466d
long12312321 <long.duc.5074@gmail.com>
2021-07-01 21:45:59 +0700

    laravel-vue

---
+++ resources/js/router.js
@@ -0,0 +1,36 @@
+import Detail from './components/Detail';
+import Home from './components/Home';
+import Login from './components/Login';
+import Register from './components/Register';
+import AdminUser from './components/AdminUser';
+
+
+export const routes = [
+
+    {
+        path:'/detail',
+        component:Detail
+    },
+    {
+        path:'/home',
+        name:'home',
+        component:Home
+    },
+    {
+        path:'/login',
+        component:Login
+    },
+    {
+        path:'/register',
+        component:Register
+    },
+    {
+        path:'/adminUser',
+        component:AdminUser
+    }
+]
+// const routers = new Router({
+//     mode:'history',
+//     router:router
+// })
+
